abfdc57ea73050c958edc16e80ec4b2fb6597f9d
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2025-10-16 08:40:44 +0800

    Update main class

--- src/Placeholdify.php
+++ src/Placeholdify.php
@@ -2,4 +2,24 @@
 
 namespace CleaniqueCoders\Placeholdify;
 
-class Placeholdify {}
+/**
+ * Placeholdify main class - acts as a factory for PlaceholderHandler
+ */
+class Placeholdify
+{
+    /**
+     * Create a new PlaceholderHandler instance
+     */
+    public static function create(): PlaceholderHandler
+    {
+        return new PlaceholderHandler;
+    }
+
+    /**
+     * Quick process method
+     */
+    public static function process(string $content, array $placeholders, string $delimiter = '{}'): string
+    {
+        return PlaceholderHandler::process($content, $placeholders, $delimiter);
+    }
+}
